9649da2c104c0001a53a2491310cb7ec9b268aa1
TheoTechStack <130162778+TheoTechStack@users.noreply.github.com>
2024-09-29 16:32:48 +0200

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//
-await builder.Build().RunAsync();
+builder.Services
+  .AddScoped(
+  sp => new HttpClient 
+  { 
+  BaseAddress = new Uri(builder.HostEnvironment.BaseAddress
+  )});
+// await on the builder.
+await builder.Build().RunAsync().ConfigureAwait(false);
